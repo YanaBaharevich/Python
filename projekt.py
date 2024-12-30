250fabf71b435e0f5d9b84f719585c382f53e68f
YanaBaharevich <w68369@student.wsiz.edu.pl>
2024-12-31 00:42:30 +0100

    Initial commit

--- projekt.py
+++ projekt.py
@@ -1,3 +1,10 @@
 import tkinter as tk
 from tkinter import messagebox
 from datetime import datetime, timedelta
+
+class Aplikacja:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Harmonogram prania")
+        self.current_step = 0
+        self.tasks = []
