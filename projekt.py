37582857ccae8bd9a0f86ca155959ea3e44e7063
YanaBaharevich <w68369@student.wsiz.edu.pl>
2024-12-31 01:28:18 +0100

    start

--- projekt.py
+++ projekt.py
@@ -1,19 +1,35 @@
 import tkinter as tk
 from tkinter import messagebox
-from datetime import datetime, timedelta
 
 
 class aplikacja:
-    def __init__(self):
-        self.root = tk.Tk()
+    def __init__(self, root):
+        self.root = root
         self.root.title("Harmonogram prania")
-        self.current_step = 0
-        self.tasks = []
 
-    def run(self):
-        self.root.mainloop()
+        self.osoba = 0
+        self.zadanie = []
+        self.osoba2 = 0
 
+        #interfeis
+        self.root.geometry("500x400")
+        self.label = tk.Label(root,text="Ilość osób chętnych:")
+        self.label.pack(pady=50)
+        self.entry = tk.Entry(root)
+        self.entry.pack(pady=20)
+        self.next_button = tk.Button(root,text="ok",command=self.ok,width=10,height=3,font=(12))
+        self.next_button.pack(pady=10)
 
-if __name__ == "__main__":
-    app = aplikacja()
-    app.run()
+    def ok(self):
+        if self.osoba == 0:
+            try:
+                self.osoba = int(self.entry.get()) #pobieranie wartości
+                if self.osoba <= 0:
+                    raise ValueError
+            except ValueError:
+                messagebox.showerror("Niepoprawna ilość.")
+                return
+
+root = tk.Tk()
+app = aplikacja(root)
+root.mainloop()
