f2e6ff3d39f8f7bb203f4fde079c861a02d2a97c
YanaBaharevich <w68369@student.wsiz.edu.pl>
2024-12-31 01:48:09 +0100

    2

--- projekt.py
+++ projekt.py
@@ -9,7 +9,7 @@ class aplikacja:
 
         self.osoba = 0
         self.zadanie = []
-        self.osoba2 = 0
+        self.indeks_osoba = 0
 
         #interfeis
         self.root.geometry("500x400")
@@ -24,11 +24,24 @@ class aplikacja:
         if self.osoba == 0:
             try:
                 self.osoba = int(self.entry.get()) #pobieranie wartości
-                if self.osoba <= 0:
+                if self.osoba <=0:
                     raise ValueError
             except ValueError:
-                messagebox.showerror("Niepoprawna ilość.")
+                messagebox.showerror("Błąd","Niepoprawna ilość.")
                 return
+            self.label.config(text=f"Imie{self.indeks_osoba+1} osoby:")
+            self.entry.delete(0, tk.END)
+            self.zadanie = []
+            self.next_button = tk.Button(root,text="ok",command=self.ok,width=10,height=3,font=(12))
+        elif len(self.zadanie) < self.osoba:
+            imie = self.entry.get().strip()
+            if not imie:
+                messagebox.showerror("Błąd","Niepoprawne imie.")
+            return
+
+            self.zadanie.append({'imie':imie,'czas':None})
+            self.indeks_osoba += 1
+
 
 root = tk.Tk()
 app = aplikacja(root)
